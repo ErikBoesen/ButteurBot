ddfa28166bd9511129cc4c9f43df5e423a90a581
ErikBoesen <me@erikboesen.com>
2022-03-09 00:56:07 -0500

    Make bot work

--- bot.py
+++ bot.py
@@ -3,13 +3,12 @@ import requests
 import mebots
 import yaledining
 import datetime
+import random
 
 
-"""
 with open('token.txt', 'r') as f:
     BOT_TOKEN = f.read().strip()
 bot = mebots.Bot('butteur_bot', BOT_TOKEN)
-"""
 dining = yaledining.API()
 
 
@@ -29,11 +28,11 @@ def send(text, bot_id):
     }
     r = requests.post(url, json=message)
 
-print(butteur_open())
+if butteur_open():
+    message = random.choice(['Butteur open', 'Butteur open !', 'Butteur open .'])
+else:
+    message = random.choice(['Butteur not open :(', 'Butteur closed'])
 
-"""
-if serving_tenders():
-    bot_ids = [instance.id for instance in bot.instances()]
-    for bot_id in bot_ids:
-        send('It is chicken tenders day today.', bot_id)
-"""
+bot_ids = [instance.id for instance in bot.instances()]
+for bot_id in bot_ids:
+    send(message, bot_id)
